d7088ae228bf0fbfd07b762b83dbc0d8869d8bc5
Shreya <shreyajaiswal2626@gmail.com>
2024-07-22 16:23:25 +0530

    commit

---
+++ dom3.js
@@ -0,0 +1,24 @@
+var arr=[
+    {
+        dp:"https://images.unsplash.com/photo-1611558709798-e009c8fd7706?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1891&q=80",
+        story:"https://plus.unsplash.com/premium_photo-1669704098750-7cd22c35422b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bW9kZWxzfGVufDB8fDB8fHww&auto=format&fit=crop&w=900&q=60"
+    },
+    {
+        dp:"https://images.unsplash.com/photo-1617922001439-4a2e6562f328?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fG1vZGVsc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=900&q=60",
+        story:"https://images.unsplash.com/photo-1625794913247-b8138fd9edb3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzR8fG1vZGVsc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60"
+    },
+    {
+        dp:"https://images.unsplash.com/photo-1612096536102-93f503aa2419?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
+        story:"https://images.unsplash.com/photo-1509631179647-0177331693ae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTQ5fHxtb2RlbHN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60"
+    },
+    {
+        dp:"https://images.unsplash.com/photo-1513379733131-47fc74b45fc7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTU3fHxtb2RlbHN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60",
+        story:""
+    }
+
+
+var clutter=""
+arr.forEach(function(){
+   clutter+= "Hello"
+})
+console.log(clutter)
